a1d474eab760fcaf61871df038ec41935b0f49da
yixhen <143676566+l1ionelw@users.noreply.github.com>
2024-07-24 00:24:10 -0600

    update: makeshift offline

--- react-client/src/firebase.js
+++ react-client/src/firebase.js
@@ -20,13 +20,13 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore();
-connectFirestoreEmulator(db, '127.0.0.1', 9098);
-export const auth = getAuth();
-connectAuthEmulator(auth, "http://127.0.0.1:9099");
+// export const db = getFirestore();
+// connectFirestoreEmulator(db, '127.0.0.1', 9098);
+// export const auth = getAuth();
+// connectAuthEmulator(auth, "http://127.0.0.1:9099");
 
 // online use
-// export const auth = getAuth(app);
-// export const db = getFirestore(app);
-//
-// const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+const analytics = getAnalytics(app);
